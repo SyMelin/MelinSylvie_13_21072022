a6556e96064e098c56d0d269c29c680d6abf17e4
Sylvie Melin <sy.melin@gmail.com>
2022-07-21 18:56:16 +0200

    work on Profile edit-form - SAVE

--- argent-bank-front/src/pages/Profile/index.jsx
+++ argent-bank-front/src/pages/Profile/index.jsx
@@ -3,6 +3,9 @@ import Account from '../../sections/Account'
 import '../../styles/Profile.css'
 
 function Profile ({ accountData }) {
+    const userFirstname = "Tony"
+    const userLastname = "Jarvis"
+    const userName = userFirstname + userLastname
 
     useEffect(() => {
         document.title = 'Argent Bank - Profile Page';
@@ -11,8 +14,25 @@ function Profile ({ accountData }) {
     return (
         <main className="main bg-dark">
             <div className="header">
-                <h1>Welcome back<br />Tony Jarvis!</h1>
+                <h1>Welcome back<br /><span>{userName}!</span></h1>
+                <form>
+                    <div className='form-inputs'>
+                        <div className="input-wrapper">
+                            <label htmlFor="userFirstname" className="sr-only">UserFirstname</label>
+                            <input type="text" id="userFirstname" placeholder="Firstname"/>
+                        </div>
+                        <div className="input-wrapper">
+                            <label htmlFor="userLastname" className="sr-only">UserLastname</label>
+                            <input type="text" id="userLastname" placeholder="Lastname"/>
+                        </div>
+                    </div>
+                    <div className='form-buttons'>
+                        <button className="form-button">Save</button>
+                        <button className="form-button">Cancel</button>
+                    </div>
+                </form>
                 <button className="edit-button">Edit Name</button>
+                
             </div>
             <h2 className="sr-only">Accounts</h2>
             {accountData.map((account, index) => (
