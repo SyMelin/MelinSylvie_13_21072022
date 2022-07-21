f1a418f9b78d0516d5548c5dc81a2ccd231edfb2
Sylvie Melin <sy.melin@gmail.com>
2022-07-21 17:12:17 +0200

    Router added

--- argent-bank-front/src/components/App/index.jsx
+++ argent-bank-front/src/components/App/index.jsx
@@ -1,19 +1,29 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Header from '../Header';
 import Home from '../../pages/Home';
 import Profile from '../../pages/Profile';
 import { accountData } from '../../data';
 import Login from '../../pages/Login';
 import Footer from '../Footer';
-import '../../styles/App.css';
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Profile accountData={accountData} />
-      <Footer />
+      <Router>
+        <Header />
+        <Routes>
+                <Route exact path="/" element={<Home />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/profile" element={<Profile accountData={accountData} />} />
+            </Routes>
+        <Footer />
+      </Router>
     </div>
   );
 }
 
 export default App;
+
+/*
+<Profile accountData={accountData} />
+*/
