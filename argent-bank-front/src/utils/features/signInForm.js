464623a0a5aadfaf5112e24021bb964bd6ba9d2c
Sylvie Melin <sy.melin@gmail.com>
2022-07-27 13:27:34 +0200

    redux-toolkit: createReducer on signInForm

--- argent-bank-front/src/utils/features/signInForm.js
+++ argent-bank-front/src/utils/features/signInForm.js
@@ -1,25 +1,12 @@
-import { createAction } from '@reduxjs/toolkit'
-import produce from 'immer'
+import { createAction, createReducer } from '@reduxjs/toolkit'
+//import produce from 'immer'
 
 const initialState = {
     email: '',
     password: '',
 }
-/*
-const SET_INPUT_VALUE = 'signIn/setInputValue'
-const SIGN_OUT = 'signIn/signOut'
 
-export const setInputValue = (id, value) => ({
-    type: SET_INPUT_VALUE,
-    payload: {
-        id: id,
-        value: value,
-    }
-})
-
-export const signInSignOut = () => ({ type: SIGN_OUT})
-*/
-
+export const signInFormSignOut = createAction('signInForm/signOut')
 export const setInputValue = createAction('signInForm/setInputValue', (id, value) => {
    return {
         payload: {
@@ -29,25 +16,15 @@ export const setInputValue = createAction('signInForm/setInputValue', (id, value
    } 
 })
 
-export const signInFormSignOut = createAction('signInForm/signOut')
-
-
-
-export default function loginReducer(state = initialState, action) {
-    return produce(state, draft => {
-        switch (action.type) {
-            case setInputValue.toString(): {
-                const id = action.payload.id;
-                draft[id] = action.payload.value
-                return
-            }
-            case signInFormSignOut.toString(): {
-                draft.email = ''
-                draft.password = ''
-                return
-            }
-            default:
-                return
-        }
+export default createReducer(initialState, builder => builder
+    .addCase(setInputValue, (draft, action) => {
+        const id = action.payload.id;
+        draft[id] = action.payload.value
+        return
     })
-}
+    .addCase(signInFormSignOut, (draft) => {
+        draft.email = ''
+        draft.password = ''
+        return
+    })
+)
